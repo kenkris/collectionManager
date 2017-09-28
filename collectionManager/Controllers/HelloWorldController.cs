79c1fdd55494ed8890253db77f1a8e6ac24f7270
Kenenth Kristensen <kehk@Kenenths-MacBook-Pro.local>
2017-09-28 13:04:38 +0200

    Add views

--- collectionManager/Controllers/HelloWorldController.cs
+++ collectionManager/Controllers/HelloWorldController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,34 @@ namespace collectionManager.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+		//
+		// GET: /HelloWorld
+		public IActionResult Index()
         {
-            return View();
+			//  View defaults to HelloWorld/Index.cshtml
+			return View();
         }
+
+		//
+		// GET: /HelloWorld/SomethingElse/
+		public string SomethingElse(string someVal, int id)
+		{
+			//  Oldschool way
+			//return HtmlEncoder.Default.Encode("Oldschool " + someVal + " " + id.ToString());
+			//  Interpolated with $
+			return HtmlEncoder.Default.Encode($"Something has this: {someVal}, and id = {id}");
+        }
+
+		public IActionResult ViewTest(string name , int repeat)
+		{
+
+			ViewData["name"] = name;
+			ViewData["repeat"] = repeat;
+
+
+			return View();
+		}
+
+
     }
 }
